6c97981624d08e33be63efc7d51f50442b1e87e1
Priyanshu <prynsu@yahoo.com>
2021-07-26 15:25:08 +0530

    Added Cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require("body-parser")
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 // DB connect
 const initializeDBConnect = require("./db/db.connect.js")
